a95439d98b13374646d6f72f0335f6db45c4edc3
Rofiqul126 <rofiqulumma@gmail.com>
2023-08-17 22:38:36 +0700

    add model for cart screen

--- lib/model/list_cart_model.dart
+++ lib/model/list_cart_model.dart
@@ -1,8 +1,14 @@
 class ListCart {
   String name;
   String price;
+  int quantity;
+  String size;
   String pathPicture;
 
   ListCart(
-      {required this.name, required this.price, required this.pathPicture});
+      {required this.name,
+      required this.price,
+      required this.pathPicture,
+      required this.quantity,
+      required this.size});
 }
