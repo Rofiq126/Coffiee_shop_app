4ca728f6e9c0a7a13faead77c0737223e034f536
Rofiqul126 <rofiqulumma@gmail.com>
2023-09-01 20:50:10 +0700

    add widget binding for get user id

--- lib/common/bottom_navigation.dart
+++ lib/common/bottom_navigation.dart
@@ -1,8 +1,10 @@
 import 'package:coffee_shop_app/common/style.dart';
 import 'package:coffee_shop_app/view/cart_screen.dart/cart_screen.dart';
 import 'package:coffee_shop_app/view/home_screen/home_screen.dart';
+import 'package:coffee_shop_app/view_model/coffee_shop_view_model.dart';
 import 'package:flutter/material.dart';
 import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:provider/provider.dart';
 
 class BottomNavBar extends StatefulWidget {
   const BottomNavBar({
@@ -16,6 +18,17 @@ class BottomNavBar extends StatefulWidget {
 class _BottomNavBarState extends State<BottomNavBar> {
   static List<Widget> optionScreen = <Widget>[const HomeScreen(), CartScreen()];
   int selectedIndex = 0;
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback(
+      (timeStamp) async {
+        var myRecipeViewModel =
+            Provider.of<CoffeeShopViewModel>(context, listen: false);
+        await myRecipeViewModel.getUid();
+      },
+    );
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
